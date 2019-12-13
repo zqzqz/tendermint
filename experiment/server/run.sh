47079b29e5a191c8354179a0ec327a9db21d67a0
zqzqz <fszqz001@sjtu.edu.cn>
2019-12-13 16:19:46 -0500

    add sleep time after node start

--- experiment/server/run.sh
+++ experiment/server/run.sh
@@ -3,6 +3,7 @@ DIR=$(dirname $0)
 
 nohup /home/ubuntu/go/bin/tendermint node --proxy_app=kvstore --trace 2>&1 > ~/.tendermint/run.log &
 echo "started"
+sleep 20
 timeout 300 bash ${DIR}/send_transaction.sh
 pkill tendermint
 echo "finished"
