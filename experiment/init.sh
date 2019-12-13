e88bc48c982ab3456b1c9133b8ebd016f50de6df
zqzqz <443291890@qq.com>
2019-12-14 03:34:14 +0800

    data

--- experiment/init.sh
+++ experiment/init.sh
@@ -1,7 +1,16 @@
 DIR=$(dirname $0)
 IP_FILE=${DIR}/aws/ips.list
 
+source ${DIR}/config.sh
+
 bash ${DIR}/utils/server_cmd.sh $IP_FILE 'rm -rf ~/.tendermint'
-bash ${DIR}/utils/server_cmd.sh $IP_FILE 'mkdir -p ~/.tendermint/config'
-bash ${DIR}/utils/server_cmd.sh $IP_FILE 'cp /home/ubuntu/go/src/github.com/tendermint/tendermint/experiment/data/genesis.json ~/.tendermint/config/genesis.json'
-bash ${DIR}/utils/server_cmd.sh $IP_FILE '/home/ubuntu/go/bin/tendermint init'
+
+IPS=$(cat $IP_FILE)
+i=0
+
+for IP in $IPS
+do
+	ssh -i ${SSH_KEY} ${USERNAME}@${IP} "cp -r /home/ubuntu/go/src/github.com/tendermint/tendermint/experiment/data/node${i} ~/.tendermint"
+	i=$(expr "$i" + 1)
+	ssh -i ${SSH_KEY} ${USERNAME}@${IP} "sed -e 's/192.198.1.${i}/${IP}/g' ~/.tendermint/config/config.toml"
+done
--- experiment/stop.sh
+++ experiment/stop.sh
@@ -6,6 +6,8 @@ RESULT_DIR=${DIR}/results
 
 bash ${DIR}/utils/server_cmd.sh $IP_FILE 'pkill /home/ubuntu/go/bin/tendermint'
 
+IPS=$(cat $IP_FILE)
+
 for IP in $IPS
 do
     scp -i ${SSH_KEY} ${USERNAME}@${IP}:/home/${USERNAME}/.tendermint/run.log ${RESULT_DIR}/${IP}.log &
