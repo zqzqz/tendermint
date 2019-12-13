09490d49bc503cd212bc10447b7654e5caef5816
zqzqz <443291890@qq.com>
2019-12-13 11:45:05 +0800

    update

--- experiment/config.sh
+++ experiment/config.sh
@@ -1,5 +1,5 @@
 USERNAME="ubuntu"
 SSH_KEY="~/.ssh/ubuntu2.pem"
 
-WORKSPACE="~/go/src/github.com/tendermint/tendermint"
-SCRIPT_DIR="~/go/src/github.com/tendermint/tendermint/experiment"
+WORKSPACE="/home/ubuntu/go/src/github.com/tendermint/tendermint"
+SCRIPT_DIR="/home/ubuntu/go/src/github.com/tendermint/tendermint/experiment"
--- experiment/server/prepare.sh
+++ experiment/server/prepare.sh
@@ -7,8 +7,8 @@ sudo add-apt-repository ppa:longsleep/golang-backports
 sudo apt-get update
 sudo apt-get install -y golang-go wget git build-essential software-properties-common curl vim
 
-echo "\nGOROOT=/usr/local/go\n" >> ~/.bashrc
-echo "\nGOPATH=/home/ubuntu/go/\n" >> ~/.bashrc
-echo '\nPATH=$GOPATH/bin:$GOROOT/bin:$PATH\n' >> ~/.bashrc
+echo "GOROOT=/usr/local/go" >> ~/.bashrc
+echo "GOPATH=/home/ubuntu/go/" >> ~/.bashrc
+echo 'PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
 
 cd $WORKSPACE && make install
